2efcbe22c87b2dfb53d0178439b3d68923fc6293
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-05-24 18:24:47 -0300

    update DialogueBox.hx

--- source/DialogueBox.hx
+++ source/DialogueBox.hx
@@ -176,8 +176,20 @@ class DialogueBox extends FlxSpriteGroup
 			startDialogue();
 			dialogueStarted = true;
 		}
+		
+		#if android
+                var justTouched:Bool = false;
 
-		if (PlayerSettings.player1.controls.ACCEPT && dialogueStarted == true)
+		for (touch in FlxG.touches.list)
+		{
+			if (touch.justPressed)
+			{
+				justTouched = true;
+			}
+		}
+		#end
+
+		if (PlayerSettings.player1.controls.ACCEPT #if android || justTouched #end && dialogueStarted == true)
 		{
 			remove(dialogue);
 				
